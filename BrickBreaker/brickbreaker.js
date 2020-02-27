f93ceab766a13aa4ea9f39058ebd355eb8d3b8b8
tikano <oadmathgenie@gmail.com>
2020-02-26 22:50:29 -0500

    Add files via upload

--- BrickBreaker/brickbreaker.js
+++ BrickBreaker/brickbreaker.js
@@ -420,8 +420,8 @@ function draw()
 			lives=lives-1;
 			if(lives == 0)    
 			{
-				PlaySoundMp3("lose1");
-				lives = -1;//game over
+				PlaySoundMp3("lose1");	//game over
+				lives = -1;	//game over
 				window.setTimeout(function(){fnAlert("GAME OVER!"); document.location.reload();},300);
 			}
 			else
